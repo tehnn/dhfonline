ab07d7eeb87efcdafccc8bce02a12ea46b250947
tehnn <tehnnn@gmail.com>
2014-02-18 10:34:56 +0700

    tehn-dhfonline

--- qry_add_pt_home.php
+++ qry_add_pt_home.php
@@ -1,5 +1,24 @@
- <div data-role="content" data-theme="f">	
-     <?php
-        print_r($_POST);        
-     ?>
- </div>
+<meta charset="UTF-8"> 
+<div data-role="content" data-theme="f">	
+    <pre>
+        <?php
+        print_r($_POST);
+        ?>
+    </pre>
+
+
+</div>
+<?php
+if (!empty($_POST)) {
+    require 'condb.php';
+    
+     echo $sql = "insert into patient_home (pid,datetime_do) values ('$_POST[pid]','$_POST[datetime_do]')";
+     mysql_query($sql);
+}
+?>
+
+
+<script>
+    alert("Add case successful!!");
+    window.location = 'pt_info.php?pid=<?= $_POST[pid] ?>';
+</script>
